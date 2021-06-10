6f825371fe9bf702d9f407fd85fb86ff8241d2f9
Prashant Shah <36514001+shahprashant030@users.noreply.github.com>
2021-06-10 21:56:54 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''
 import Adafruit_DHT
  
 # Set sensor type : Options are DHT11,DHT22 or AM2302
@@ -16,3 +17,20 @@ if humidity is not None and temperature is not None:
   print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
 else:
   print('Failed to get reading. Try again!')
+  '''
+import time
+import board
+import adafruit_dht
+#Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT11(board.D17)
+while True:
+    try:
+         # Print the values to the serial port
+         temperature_c = dhtDevice.temperature
+         temperature_f = temperature_c * (9 / 5) + 32
+         humidity = dhtDevice.humidity
+         print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% "
+               .format(temperature_f, temperature_c, humidity))
+    except RuntimeError as error:     # Errors happen fairly often, DHT's are hard to read, just keep going
+         print(error.args[0])
+    time.sleep(2.0)
